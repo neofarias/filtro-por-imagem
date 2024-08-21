71ac9ed026fcba769ce778725b2ac38539be2dff
neofarias <marciofsctt@gmail.com>
2024-08-21 19:16:04 -0300

    Ajuste no import do Pexels

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import {createClient} from 'pexels';
-
+import { createClient } from 'pexels';
 const client = createClient('pXXo6ot2FU6CShvvpjQPBVowtdZIYEGs5ImcH37w0OrsaeA2POvAchIb');
 
+const query = 'Nature';
+const listaFotos = [];
+
+client.photos.search({ query, per_page: 1 }).then(photos => listaFotos);
+console.log(listaFotos);
