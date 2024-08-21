c8e96590a87dec9ff97a49f4998c033a1b5741e0
neofarias <marciofsctt@gmail.com>
2024-08-21 19:41:43 -0300

    Testes em código existente

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { createClient } from 'pexels';
 const client = createClient('pXXo6ot2FU6CShvvpjQPBVowtdZIYEGs5ImcH37w0OrsaeA2POvAchIb');
 
-const query = 'Nature';
-const listaFotos = [];
+const query = "nature";
+let listaFotos = [];
 
-client.photos.search({ query, per_page: 1 }).then(photos => listaFotos);
+client.photos.search({ query, per_page: 1 }).then(photos => {
+    listaFotos[{...photos}];
+});
+
+const resultJson = JSON.stringify(listaFotos);
+
+console.log(resultJson);
 console.log(listaFotos);
