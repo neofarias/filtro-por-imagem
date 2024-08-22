73f2297e973fc3f994c55bf0afbc7192087fec02
neofarias <marciofsctt@gmail.com>
2024-08-22 12:00:25 -0300

    Tentativa de utilizar o listaFotos fora do escopo

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import { createClient } from 'pexels';
+
 const client = createClient('pXXo6ot2FU6CShvvpjQPBVowtdZIYEGs5ImcH37w0OrsaeA2POvAchIb');
-
 const query = "nature";
-let listaFotos = [];
 
-client.photos.search({ query, per_page: 1 }).then(photos => {
-    listaFotos[{...photos}];
-});
+let listaFotos = {};
 
-const resultJson = JSON.stringify(listaFotos);
+client.photos.search({ query, per_page: 1 }).then(photosResult => {
+    listaFotos = {...photosResult};
+    //console.log(listaFotos); //Mostra lindo o resultado
+ });
 
-console.log(resultJson);
-console.log(listaFotos);
+console.log(listaFotos); // Fica vazio!!!\
